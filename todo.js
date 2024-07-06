6fa84ca64951807585a46b80a232274cc13e81f8
Raghav Khanna <raghavkhanna@Raghavs-MacBook-Pro.local>
2024-07-06 16:40:23 +0530

    added drag and drop feature for the sticky notes

--- todo.js
+++ todo.js
@@ -60,7 +60,61 @@ sticky.addEventListener('click', (e) => {
     <div class="note-cont">
         <textarea></textarea>
     </div>
-    `
-
+    `;
     document.body.appendChild(stickyCont);
-})
+
+    let minimize = stickyCont.querySelector(".minimize");
+    let remove = stickyCont.querySelector(".remove");
+    // noteActions(minimize, remove, stickyCont);
+
+    stickyCont.onmousedown = function(event) {
+        dragAndDrop(stickyCont, event);
+    };
+      
+    stickyCont.ondragstart = function() {
+        return false;
+    };
+});
+
+// function noteActions(minimize, remove, stickyCont) {
+//     console.log("inside notes action", remove);
+
+//     remove.addEventListener('click', (e) => {
+//         console.log("clicked", stickyCont);
+//         stickyCont.remove();
+//     });
+//     // minimize.addEventListener('click', (e) => {
+
+//     // })
+// }
+
+function dragAndDrop(element, event) {
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+    let shiftY = event.clientY - element.getBoundingClientRect().top;
+  
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+    document.body.append(element);
+  
+    moveAt(event.pageX, event.pageY);
+  
+    // moves the element at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+      element.style.left = pageX - shiftX + 'px';
+      element.style.top = pageY - shiftY + 'px';
+    }
+  
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+  
+    // move the element on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+  
+    // drop the element, remove unneeded handlers
+    element.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      element.onmouseup = null;
+    };
+}
